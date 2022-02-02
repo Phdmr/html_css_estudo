c31e2dc4c218a8c078195403aa0f4bf8c9ab8fc5
Pedro Rocha <33499845+Phdmr@users.noreply.github.com>
2022-02-01 21:11:15 -0300

    Utilização do display grid

--- flex-e-grid-main/index.js
+++ flex-e-grid-main/index.js
@@ -1,19 +1,25 @@
-const botaoMenu = document.querySelector('.cabecalho__menu')
+const botaoMenu = document.querySelector('.cabecalho__menu');
 
-const menu = document.querySelector('.menu-lateral')
-
-const botIco = document.querySelector('.menu-lateral__link')
+const menu = document.querySelector('.menu-lateral');
 
 botaoMenu.addEventListener('click', () => {
     menu.classList.toggle('menu-lateral--ativo')
 
 })
 
-document.querySelectorAll('.menu-latera__link').forEach(item =>{
-    botIco.addEventListener('click', () => {
-        botIco.classList.toggle('menu-lateral__link--ativo')
-    })
-})
+var botoes = menu.querySelector('.menu-lateral__link');
+
+for (var i = 0; i < botoes.length; i++) {
+    botoes[i].addEventListener('click', () => {
+        var current = document.getElementsByClassName('.menu-lateral__link--ativo');
+        if (current.length > 0) { (current[0].className = current[0].className.replace('.menu-lateral__link--ativo',''))
+            }
+        this.className += '.menu-lateral__link--ativo';
+        
+        })
+}
+
+
 
 
 
